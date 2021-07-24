e46570e25b4b7e5d374f8d46aad4a1ecdaf4a75c
Nitish Kaushik <nkaushik971230.com>
2021-07-24 13:41:59 +0530

    Initial Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container-fluid">
+            Welcome! This is an about component.
+            <p>
+                By keeping such a list, you make sure that your tasks are written down all in one place so you don't forget anything important. And by prioritizing tasks, you plan the order in which you'll do them, so that you can tell what needs your immediate attention, and what you can leave until later.
+                <br />Good Luck!
+            </p>
+        </div>
+    )
+}
